25347691f07269dbdabb9ef71ed2ff2cbb167dcb
JordanHsieh <jordan.y.hsieh@gmail.com>
2015-03-21 17:54:01 -0700

    fixed gridster

--- js/controllers/dashboard.js
+++ js/controllers/dashboard.js
@@ -10,16 +10,81 @@
 
 // var APP = angular.module('myappApp');
 
-APP.controller('DashboardCtrl', function ($scope) {
+APP.controller('DashboardCtrl', function ($scope, $timeout, $mdBottomSheet) {
 
-	console.log("hello");
-
-
+	// to initialize and get the gridster working
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140]
     });
- 
 
+    window.onload = function(){
+    	console.log("page loading");
+	};
+
+    var showGrid = 0;
+    // setting up angular material bottom sheet
+    $scope.showGridBottomSheet = function($event) {
+    	console.log(showGrid);
+    	if(showGrid == 1)
+    	{
+		    $scope.alert = '';
+		    $mdBottomSheet.show({
+		      templateUrl: 'views/bottom-sheet-grid-template.html',
+		      controller: 'GridBottomSheetCtrl',
+		      targetEvent: $(event)
+		    }).then(function(clickedItem) {
+		      $scope.alert = clickedItem.name + ' clicked!';
+		      if(clickedItem.name == '1 x 1')
+		      {
+		      	addWidget(1, 1);
+		      }
+		      else if(clickedItem.name == '2 x 1')
+		      {
+		      	addWidget(2, 1);
+		      }
+		      else if(clickedItem.name == '1 x 2')
+		      {
+		      	addWidget(1, 2);
+		      }
+		    });
+		    showGrid = 0;
+		}
+		else
+		{
+			showGrid++;
+		}
+  	};
+
+  	var gridster = $(".gridster ul").gridster().data('gridster');
+
+  	function addWidget(sizeX, sizeY)
+  	{
+  		gridster.add_widget('<li class="new">The HTML of the widget...</li>', sizeX, sizeY);
+  	}
+
+});
+
+APP.controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
+  // $scope.items = [
+  //   { name: 'Hangout', icon: 'hangout' },
+  //   { name: 'Mail', icon: 'mail' },
+  //   { name: 'Message', icon: 'message' },
+  //   { name: 'Copy', icon: 'copy' },
+  //   { name: 'Facebook', icon: 'facebook' },
+  //   { name: 'Twitter', icon: 'twitter' },
+  // ];
+
+
+	$scope.items = [
+		{ name: '1 x 1', icon: '1x1' },
+		{ name: '2 x 1', icon: '2x1' },
+		{ name: '1 x 2', icon: '1x2' },
+	];
+
+	$scope.listItemClick = function($index) {
+		var clickedItem = $scope.items[$index];
+		$mdBottomSheet.hide(clickedItem);
+	};
 });
 
