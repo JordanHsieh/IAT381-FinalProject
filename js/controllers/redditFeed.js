84d45ca1f12f0ba53403ab877e041a4b71b6432c
JordanHsieh <jordan.y.hsieh@gmail.com>
2015-03-23 22:56:31 -0700

    adding some reddit code

--- js/controllers/redditFeed.js
+++ js/controllers/redditFeed.js
@@ -10,21 +10,33 @@
 
 // var APP = angular.module('myappApp');
 
-APP.controller('RedditFeedCtrl', function ($scope) {
+APP.controller('RedditFeedCtrl', function ($scope, $http) {
 
-	var url = "http://www.reddit.com/r/jokes.json?jsonp=JSON_CALLBACK";
+	$scope.url = "http://www.reddit.com/r/jokes.json?jsonp=JSON_CALLBACK";
 
-	$http.jsonp(url)
-    .success(function(redditData){
-    	$scope.redditjoke = redditData.data.children[0].data;
-   
-    });
+	$scope.fetch = function() {
+		$scope.code = null;
+		$scope.response = null;
 
-	$.getJSON("http://www.reddit.com/r/jokes/.json?jsonp=?", function(data) { 
-	    // $.each(data.data.children, function(i,item){
-	    //     $("<img/>").attr("src", item.data.url).appendTo("#images");
-	    // });
-		console.log(data);
-	});
+		$http.jsonp($scope.url)
+	    	.success(function(redditData, status){
+		    	$scope.status = status;
+		    	$scope.redditjoke = redditData.data.children[0].data;
+		   		console.log($scope.redditjoke);
+	    	}).
+	    	error(function(redditData, status) {
+	    		$scope.redditData = redditData || "Request failed";
+	    		$scope.status = status;
+		});
+	}
+
+	
+
+	// $.getJSON("http://www.reddit.com/r/jokes/.json?jsonp=?", function(data) { 
+	//     // $.each(data.data.children, function(i,item){
+	//     //     $("<img/>").attr("src", item.data.url).appendTo("#images");
+	//     // });
+	// 	console.log(data);
+	// });
 
 });
