252f5122d621a7cd99f64b4d13f63b4316728e44
trixpama <trixpama@gmail.com>
2015-03-31 17:08:39 -0700

    top joke shows up on card

--- js/controllers/joke-of-the-day.js
+++ js/controllers/joke-of-the-day.js
@@ -14,26 +14,39 @@ APP.controller('JokeCtrl', function ($scope, $http) {
 
 	$scope.url = "http://www.reddit.com/r/jokes.json?jsonp=JSON_CALLBACK";
 
-	$scope.fetch = function() {
-		$scope.code = null;
-		$scope.response = null;
-
-		$http.jsonp($scope.url)
-	    	.success(function(redditData, status){
-		    	$scope.status = status;
-		    	$scope.redditjoke = redditData.data.children[0].data;
-		   		console.log($scope.redditjoke);
-	    	}).
-	    	error(function(redditData, status) {
-	    		$scope.redditData = redditData || "Request failed";
-	    		$scope.status = status;
-		});
-	}
-
-	reddit.hot('aww').limit(5).fetch(function(res) {
+reddit.hot('jokes').limit(1).fetch(function(redditData) {
     // res contains JSON parsed response from Reddit
-    	console.log(res);
+    	$scope.redditJokes = redditData.data.children;
+    	$scope.$apply();
+    	console.log($scope.redditJokes);
   	});
+
+  	$scope.jokeInfo = function() {
+  		var joke = this.joke;
+  		console.log(joke);
+  	}
+
+
+	// $scope.fetch = function() {
+	// 	$scope.code = null;
+	// 	$scope.response = null;
+
+	// 	$http.jsonp($scope.url)
+	//     	.success(function(redditData, status){
+	// 	    	$scope.status = status;
+	// 	    	$scope.redditjoke = redditData.data.children[0].data;
+	// 	   		console.log($scope.redditjoke);
+	//     	}).
+	//     	error(function(redditData, status) {
+	//     		$scope.redditData = redditData || "Request failed";
+	//     		$scope.status = status;
+	// 	});
+	// }
+
+	// reddit.hot('aww').limit(5).fetch(function(res) {
+ //    // res contains JSON parsed response from Reddit
+ //    	console.log(res);
+ //  	});
 
 	$scope.clickCard = function(e) {
 		var ct = $(e.currentTarget);
