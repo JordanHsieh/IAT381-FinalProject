043a62ea1da3000e5ad6404724c955d131aa5c60
kennethlai21 <kla83@sfu.ca>
2015-04-05 22:26:26 -0700

    cardstack test

---
+++ js/controllers/cardstack.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  function reorderCards() {
+    $('.jokecard').css('z-index','999999');
+      var i = 0;
+  $('.jokecard').each(function(){
+    $(this).css({
+      'transform': 'translateX(' + i +'px ) translateY(' + i +'px )',
+      'z-index': 999999 - i
+    });    
+    i = i + 5;
+  });
+    
+  $('.jokecard').removeClass('top-card');
+    $('.jokecard').first().addClass('top-card');
+  }
+  
+  
+  $('.cardstack').on('click','.top-card',function(){
+    var thisCard = $(this);
+    $(this).animate({
+      'top':-430}, 500,  function(){
+      thisCard.detach().appendTo('ul.cardstack');
+        reorderCards();
+        $(this).animate({
+          'top': 0
+        })
+      });
+      
+
+  });
+  
+  reorderCards();
+});
