9a2e1443b4ea6c1ce2ab292b9d06ae8046cbbfa7
JordanHsieh <jordan.y.hsieh@gmail.com>
2015-04-09 23:50:54 -0700

    added time to new jokes

--- js/controllers/newJokes.js
+++ js/controllers/newJokes.js
@@ -17,12 +17,15 @@ APP.controller('NewJokesCtrl', function ($scope, $http, DbService) {
 	DbService.updateFavorites();
   DbService.runDb();
 
+  
+  console.log($scope.created_utc);
 
   $scope.url = "http://www.reddit.com/r/jokes.json?jsonp=JSON_CALLBACK";
 
   reddit.new('jokes').limit(10).fetch(function(redditData) {
     // res contains JSON parsed response from Reddit
       $scope.newFilteredList = [];
+      APP.newFilteredList = $scope.newFilteredList;
       $scope.redditJokes = redditData.data.children;
 
       for (var i = 0; i<$scope.redditJokes.length; i++) {
@@ -30,7 +33,14 @@ APP.controller('NewJokesCtrl', function ($scope, $http, DbService) {
           var newJoke = $scope.redditJokes[i];
           var jokeTitle = newJoke.data.title;
           var jokeText = newJoke.data.selftext;
-          // var jokeTime = newJoke.data.submitted;
+          var jokeTime = newJoke.data.created_utc
+
+          var utcSeconds = jokeTime;
+          var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+          d.setUTCSeconds(utcSeconds);
+
+          newJoke.data['time'] = String(d);
+
           var jokeLength = jokeTitle.length + jokeText.length;
 
           if (jokeLength <= 400) {
@@ -39,13 +49,12 @@ APP.controller('NewJokesCtrl', function ($scope, $http, DbService) {
           }
       }
 
+      console.log($scope.newFilteredListTime);
+
       $scope.$apply();
       // console.log($scope.redditJokes);
   });
 
-  $scope.swipeJokeRight = function(e) {
-    console.log('swipe right');
-  }
 
   // $scope.addJoke = function(e) {
  //    var joke = this.joke;
